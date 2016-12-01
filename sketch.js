8fe951074e8bd0f66dcc44cb3565253ab1c6f241
xonatialee <xonatialee@gmail.com>
2016-12-01 18:27:21 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+var x = [],
+  y = [],
+  angle = [],
+  segNum = 20,
+  segLength = 10, 
+  //speed
+  ballX = 50,
+  ballY = 50,
+  ballXDirection = 2,
+  ballYDirection = 3;
+
+for (var i = 0; i < segNum; i++) {
+  x[i] = 0;
+  y[i] = 0;
+  angle[i] = 0;
+}
+
+function setup() {
+  createCanvas(710, 400);
+  strokeWeight(9);
+  stroke(255, 100);
+}
+
+function draw() {
+  background(0);
+  dragSegment(0, mouseX, mouseY);
+  for( var i=0; i<x.length-1; i++) {
+    dragSegment(i+1, x[i], y[i]);
+  }
+  
+  strokeWeight(20);
+  ballX = ballX + 1.0 * ballXDirection;
+  ballY = ballY + 0.8 * ballYDirection;
+  if(ballX > width-25 || ballX < 25) {
+    ballXDirection *= -1;
+  }
+  if(ballY > height-25 || ballY < 25) {
+    ballYDirection *= -1;
+  }
+  ellipse(ballX, ballY, 10, 10);
+
+}
+
+function dragSegment(i, xin, yin) {
+  var dx = xin - x[i];
+  var dy = yin - y[i];
+  var angle = atan2(dy, dx);
+  x[i] = xin - cos(angle) * segLength;
+  y[i] = yin - sin(angle) * segLength;
+  segment(x[i], y[i], angle);
+}
+
+function segment(x, y, a) {
+  push();
+  translate(x, y);
+  rotate(a);
+  line(0, 0, segLength, 0);
+  pop();
+}
+
+//function called collison
+//check to see if the the mousex and mousey is with 5 pixels of the ball
+//the mosue x <= ball locationX + half the size of the ball, ie. 5
+//th mouse y <= ball lcoationY + half the size of the ball, ie. 5
+//if true console hit
+function collision() {
+  
+}
